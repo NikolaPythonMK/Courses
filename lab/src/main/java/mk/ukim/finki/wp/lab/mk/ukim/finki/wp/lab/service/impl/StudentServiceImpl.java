16e58da5d02d6225a31f2e4daf7cba443492cee0
NikolaPythonMK <76405057+NikolaPythonMK@users.noreply.github.com>
2022-11-16 16:18:14 +0100

    update

---
+++ lab/src/main/java/mk/ukim/finki/wp/lab/mk/ukim/finki/wp/lab/service/impl/StudentServiceImpl.java
@@ -0,0 +1,53 @@
+package mk.ukim.finki.wp.lab.mk.ukim.finki.wp.lab.service.impl;
+
+import mk.ukim.finki.wp.lab.mk.ukim.finki.wp.lab.model.Student;
+import mk.ukim.finki.wp.lab.mk.ukim.finki.wp.lab.repository.StudentRepository;
+import mk.ukim.finki.wp.lab.mk.ukim.finki.wp.lab.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    private final StudentRepository repository;
+
+    @Autowired
+    public StudentServiceImpl(StudentRepository repo) {
+        this.repository = repo;
+    }
+
+    @Override
+    public List<Student> listAll() {
+        return repository.findAllStudents();
+    }
+
+    @Override
+    public List<Student> searchByNameOrSurname(String text) {
+        return repository.findAllByNameOrSurname(text);
+    }
+
+    @Override
+    public Student save(String username, String password, String name, String surname) {
+        Student student = repository.findAllStudents().stream()
+                .filter(i -> i.getSurname().equals(username)).findFirst().orElse(null);
+
+        boolean validName = name != null && !name.isEmpty();
+        boolean validSurname = surname != null && !surname.isEmpty();
+
+        if(student == null && validName && validSurname){
+            Student savedStudent = new Student(username, password, name, surname);
+            StudentRepository.students.add(savedStudent);
+            return savedStudent;
+        }
+        return null;
+    }
+
+    public List<Student> searchBySubstring(String substring){
+        if(substring == null || substring.isEmpty()){
+           throw new NullPointerException();
+        }
+        return repository.searchBySubstring(substring);
+    }
+}
